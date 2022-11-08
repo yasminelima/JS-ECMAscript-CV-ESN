0e06116fc730aa4e6de31cc0740f0a58cace5b16
Yasmine Lima <yasminelima.dev@gmail.com>
2022-11-08 16:20:53 -0300

    Create ambiente.js

---
+++ aulas/aula013/ambiente.js
@@ -0,0 +1,18 @@
+var c = 1
+while (c <= 6) {
+    console.log (`Passo ${c}`)
+    c++
+}
+
+
+
+/*
+console.log ('Tudo bem?')
+console.log ('Tudo bem?')
+console.log ('Tudo bem?')
+console.log ('Tudo bem?')
+console.log ('Tudo bem?')
+console.log ('Tudo bem?')
+*/
+
+
